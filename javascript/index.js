2621a188c961cf699c4c5e99f1b47af22fa42b79
Barbara559 <b7035031@gmail.com>
2024-12-13 13:23:21 +0100

    predavanje

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+/*var broj1 =prompt("Unesi prvi broj");
+var broj2 =prompt("Unesi drugi broj");
+if(broj1 > broj2)
+{
+    alert(broj1 + "je veći od broja" +broj2);
+}
+else if (broj1 < broj2) {
+    
+}
+else {
+    aler("Brojevi su jednaki");
+} */
+// Love Claculator
+var ime1 = prompt ("Unesi svoje ime: ");
+var ime2 = prompt("Unesi ime simpatije: ")
+var n = Math.floor (Math.random()*100) + 1;
+alert ("Kompatibilni ste:" + n + "%");
